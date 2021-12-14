27de49598b021738646524b4df1ce064c08c7c01
itaitan Araujo <itaitan2@gmail.com>
2021-12-14 14:49:51 -0300

    Colocando Icons, e tratamento de erro

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,6 +1,10 @@
-import { CoursesService } from './../services/courses.service';
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { catchError, Observable, of } from 'rxjs';
+
 import { Course } from '../model/course';
+import { ErrorDialogComponent } from './../../shared/components/error-dialog/error-dialog.component';
+import { CoursesService } from './../services/courses.service';
 
 @Component({
   selector: 'app-courses',
@@ -8,15 +12,30 @@ import { Course } from '../model/course';
   styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
-  courses: Course[] = [];
+  courses$: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
   //coursesService: CoursesService;
-  constructor(private coursesService: CoursesService) {
+
+  constructor(
+    public dialog: MatDialog,
+    private coursesService: CoursesService
+  ) {
     //this.coursesService = new CoursesService();
-    this.courses = this.coursesService.list();
+    //this.courses = this.coursesService.list();
+
+    this.courses$ = this.coursesService.list().pipe(
+      catchError((error) => {
+        this.onError('Erro ao carregar cursos.');
+        return of([]);
+      })
+    );
   }
 
-  ngOnInit(): void {
-    this.courses = this.coursesService.list();
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg,
+    });
   }
+
+  ngOnInit(): void {}
 }
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,14 +1,22 @@
 import { Course } from './../model/course';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { delay, first, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CoursesService {
+  private readonly API = '/assets/courses.json';
   constructor(private httpClient: HttpClient) {}
 
-  list(): Course[] {
-    return [{ _id: '1', name: 'Angular', category: 'Front-end' }];
+  list() {
+    return this.httpClient.get<Course[]>(this.API).pipe(
+      first(),
+      //delay(3000),
+      tap((courses) => {
+        console.log(courses);
+      })
+    );
   }
 }
