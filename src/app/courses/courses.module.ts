706fdf31ed0a282bff2e6b40265a84e1213a7d1b
itaitan Araujo <itaitan2@gmail.com>
2021-12-13 22:41:40 -0300

    Testando Courses em  Http

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,9 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
-import { AppMaterialModule } from '../shared/app-material/app-material.module';
 
 @NgModule({
   declarations: [CoursesComponent],
--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,3 +1,4 @@
+import { CoursesService } from './../services/courses.service';
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
 
@@ -7,15 +8,15 @@ import { Course } from '../model/course';
   styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
-  courses: Course[] = [
-    {
-      _id: '1',
-      name: 'Angular',
-      category: 'Front-end',
-    },
-  ];
+  courses: Course[] = [];
   displayedColumns = ['name', 'category'];
-  constructor() {}
+  //coursesService: CoursesService;
+  constructor(private coursesService: CoursesService) {
+    //this.coursesService = new CoursesService();
+    this.courses = this.coursesService.list();
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.courses = this.coursesService.list();
+  }
 }
